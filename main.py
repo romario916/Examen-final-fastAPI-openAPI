a14d09ebd8cd062adbc62b6b568e6ff35a44a40f
romario916 <hei.henri.2@gmail.com>
2025-09-03 12:17:28 +0300

    api

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-from fastapi import FastAPI
+from http.client import HTTPException
+from typing import List
 
-# Création de l’application FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+
 app = FastAPI()
 
-# Route GET
-@app.get("/hello")
-def read_root():
-    return {"message": "Hello romario"}
+@app.get("/ping")
+def hello():
+    return "pong"
+
+
+
+class Car(BaseModel):
+    id: str
+    brand: str
+    model: str
+    maxSpeed: int
+    maxFuelCapacity:int
+
+
+voiture: List[Car] = []
+
+
+@app.get("/cars", response_model=List[Car])
+def get_car():
+    return voiture
+
+
+@app.post("/cars", response_model=Car)
+def create_car(voiture: Car):
+    voiture.append(voiture)
+    return voiture
+
+@app.get("/etudiants/{id}", response_model=Car)
+def update_car(id: int, updated_car: Car):
+    for i, e in enumerate(voiture):
+        if e.id == id:
+            voiture[i] = updated_car
+            return updated_car
+    raise HTTPException(status_code=404, detail="car non trouvé")
+
